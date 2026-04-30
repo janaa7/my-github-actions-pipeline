9d92937e2bb60c1f0eb7ff3a2995d7c0a4ce77f5
janaa <jana@gmail.com>
2026-04-30 11:24:00 +0300

    Initial commit

---
+++ my_function.py
@@ -0,0 +1,3 @@
+# my_function.py
+def add(a, b):
+    return a + b
---
+++ test_my_function.py
@@ -0,0 +1,13 @@
+# test_my_function.py
+import unittest
+from my_function import add
+
+class TestMyFunction(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
